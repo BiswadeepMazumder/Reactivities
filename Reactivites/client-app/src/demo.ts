cd1c99925cdba8c57211fb540891b3c6cfa65c81
BiswadeepMazumder <biswadeepmazumder96@gmail.com>
2022-09-25 20:56:55 -0400

    Type Script addition

---
+++ Reactivites/client-app/src/demo.ts
@@ -0,0 +1,10 @@
+const duck1={
+    name:'huey',
+    numLegs:2, makesound: (sound: any)=>console.log(sound)
+}
+
+const duck2={
+    name:'juey',
+    numLegs:2, makesound: (sound: any)=>console.log(sound)
+}
+
